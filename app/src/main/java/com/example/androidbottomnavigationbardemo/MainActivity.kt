ecb654cb5940f1695ff28e33ddf7c151b44677cc
Vladislav Trofimov <vl.an.trofimov@gmail.com>
2022-06-09 00:19:26 +0600

    first commit

---
+++ app/src/main/java/com/example/androidbottomnavigationbardemo/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.androidbottomnavigationbardemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import com.example.androidbottomnavigationbardemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+
+}
